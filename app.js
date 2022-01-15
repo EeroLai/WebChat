7484c9ae94ef11f11286e2567420b57a6ca05b42
EeroLai <84887419+EeroLai@users.noreply.github.com>
2022-01-15 22:18:44 +0800

    Signed-off-by: EeroLai

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs');
 
 // set layouts
 app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,12 +29,12 @@ app.use('/', webRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
