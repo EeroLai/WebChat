8db91cf348692ffe5a0b069e52888465a1a5e490
EeroLai <84887419+EeroLai@users.noreply.github.com>
2022-10-13 21:08:41 +0800

    fix layout

--- Front-End/nuxt-app/layouts/default.vue
+++ Front-End/nuxt-app/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex h-screen justify-center">
-      <div class="bg-gray-600 w-1/3">
+      <div class="bg-gray-600 w-1/4">
         <BaseHeader />
   
         <div class="h-[90%] p-3 bg-gradient-to-r from-slate-700 via-slate-600 to-slate-700">
